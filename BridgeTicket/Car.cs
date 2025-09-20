4fe458a538a85a92a15841c9bb23eb133aed2abb
Tobias Bech Knudsen <tok005@edu.zealand.dk>
2025-09-20 23:19:40 +0200

    Add project files.

---
+++ BridgeTicket/Car.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace BridgeTicket;
+
+public class Car
+{
+
+	/// <summary>
+	/// Denne her property bruger til identifikation af bilen
+	/// </summary>
+	public string Licenseplate { get; set; }
+	/// <summary>
+	/// Det bruger vi for at anse tidspunktet af bilen kører over broen
+	/// </summary>
+	public DateTime Date { get; set; }
+
+	/// <summary>
+	/// Bruger vi for at sikre at prisen vil være fast
+	/// </summary>
+	/// <returns></returns>
+	public double Price()
+	{
+		return 230;
+	}
+
+	/// <summary>
+	/// Bruger vi senere til at override med noget polymorfi
+	/// </summary>
+	/// <returns></returns>
+	public string Type()
+	{
+		return "Car";
+	}
+
+	
+
+
+}
---
+++ BridgeTicketTests/CarTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BridgeTicket;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace BridgeTicketTests;
+
+
+[TestClass()]
+public class CarTests
+{
+
+	// Tester at Price returnerer den korrekte faste pris
+	[TestMethod]
+	public void Price_ReturnsFixedValue()
+	{
+		// Arrange: Opretter en bil
+		var car = new Car();
+
+		// Act: Kalder Price()
+		double price = car.Price();
+
+		// Assert: Tjekker at prisen er 230
+		Assert.AreEqual(230.0, price);
+	}
+
+
+}
